ff1d5ba827330544d8324bbcbf4a54f435d2c30e
msurmava <msurmava@unisens.ge>
2022-09-19 00:36:30 +0400

    some specs added

--- spec/models/research_spec.rb
+++ spec/models/research_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Research, type: :model do
+  let(:research) {FactoryBot.build(:research)}
+
+  context 'validates' do
+    it { expect(research.name).to be_kind_of(String)}
+
+    it { expect(research).to be_valid }
+
+    it { expect(research.companies).not_to be(nil)}
+  end
 end
